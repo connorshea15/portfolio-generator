65168a62ac35d367f5753c55a3c3fdad7abe0be2
connorshea15 <connorshea15@gmail.com>
2020-07-22 16:35:22 -0700

    Dynamically create about section

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {  
     return inquirer.prompt([
@@ -117,13 +119,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-      console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
+
+      fs.writeFile('./index.html', pageHTML, err => {
+           if (err) throw new Error(err);
+
+           console.log('Page created! Check out index.html in this directory to see it!');
+      });
     });
     // .then(projectAnswers => console.log(projectAnswers));    
 
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+
 
 // This is just a reference for one way to get user input from the command prompt
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
