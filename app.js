d65331982d2143d62ba43945267548fdae77841f
connorshea15 <connorshea15@gmail.com>
2020-07-21 17:54:25 -0700

    Inquirer is working like a charm

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// This is just a reference for one way to get user input from the command prompt
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-    console.log('Portfolio Complete! Check out index.html to see the output!');
-});
+// const [name, github] = profileDataArgs;
+
+// const pageHTML = generatePage(portfolioData);
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio Complete! Check out index.html to see the output!');
+// });
