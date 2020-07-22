33409ab82ce3455374b23b1b21bb3a6641338019
connorshea15 <connorshea15@gmail.com>
2020-07-22 14:20:51 -0700

    the questions for the user are noce

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ const promptUser = () => {
     ]);
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
+  // If there;s no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  
     console.log(`
   =================
   Add a New Project
@@ -60,13 +65,22 @@ const promptProject = () => {
         message: 'Would you like to enter another project?',
         default: false
       }
-    ]);
+    ]).then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
   };
 
 promptUser()
-    .then(answers => console.log(answers))
     .then(promptProject)
-    .then(projectAnswers => console.log(projectAnswers));    
+    .then(portfolioData => {
+      console.log(portfolioData);
+    });
+    // .then(projectAnswers => console.log(projectAnswers));    
 
 
 // const fs = require('fs');
